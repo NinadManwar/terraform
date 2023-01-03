7179ffa10da41cb76c7cc3b9d7b129e6dedd15b0
Ninad Manwar <92081490+NinadManwar@users.noreply.github.com>
2023-01-03 16:16:05 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
     region = "us-east-1"
-    access_key= "AKIAXM47W6CG6T3XOTFR"
-    secret_key= "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"
+    access_key= ""
+    secret_key= ""
 }
